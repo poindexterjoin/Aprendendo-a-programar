8d05f63720e3da4aa570af84b1d3c7e7abbab943
poindexterjoin <edson.a.silva@outlook.com>
2024-10-17 20:51:50 -0300

    Add files via upload
    Meu projeto

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const elementos=document.querySelectorAll('[data-anima]');
+const animacaoClass='animacao';
+
+function animaScroll() {
+	const toPaginaNaJanela=window.pageYOffset+((window.innerHeight*3)/4); // 3/4 da janela
+	elementos.forEach(function(elemento){
+		if(toPaginaNaJanela > elemento.offsetTop){
+			elemento.classList.add(animacaoClass);
+		}else{
+			elemento.classList.remove(animacaoClass);
+		}
+	});
+}
+if(elementos.length){
+	window.addEventListener('scroll',function(){
+		animaScroll();
+	})
+}
+
+function cliqueModal(img){
+	const janelaModal=document.getElementById('janelaModal');
+	const imgModal=document.getElementById('imgModal');
+	const txtModal=document.getElementById('txtModal');
+	const btnFechar=document.getElementById('btnFechar');
+		janelaModal.classList.remove("escondeJanelaModal");
+		janelaModal.classList.add("mostraJanelaModal");
+		imgModal.src=img.src;
+		imgModal.alt=img.alt;
+		txtModal.innerHTML=img.alt;
+
+		btnFechar.onclick=function(){
+		janelaModal.classList.add("escondeJanelaModal");
+		janelaModal.classList.remove("mostraJanelaModal");
+	} 
+}
